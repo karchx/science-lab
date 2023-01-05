b75683f4e3b8f773ebad626a20aff44b5d8c2d44
karchx <kenethra.74@gmail.com>
2023-01-05 01:06:58 +0000

    feat: create user

--- start.sh
+++ start.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
+
+user="stivscience"
+fullname="karchx"
+
 echo "start"
 echo "welcome to lab"
+
+configure_user() {
+	# SET YOUR USER!
+	if id $user &> /dev/null; then
+		echo "user exists"
+	else
+		echo "configuring user..."
+		useradd -c $fullname -m $user
+		passwd $user
+	fi
+}
 
 command_exists() {
 	type "$1" &> /dev/null;
@@ -9,6 +24,10 @@ command_exists() {
 install_package_nix() {
 	nix-env -f '<nixpkgs>' -iA $1
 }
+
+configure_user
+
+sleep 1
 
 if ! command_exists vim;
 then
