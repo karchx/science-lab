3f802ff3278045c1fc20693be787e57929c500f6
karchx <kenethra.74@gmail.com>
2023-01-07 23:31:04 +0000

    refactor: system module

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -4,7 +4,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./modules/hyprland/default.nix
+      ../hyprland/default.nix
     ];
 
   # Use the GRUB 2 boot loader.
