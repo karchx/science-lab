57d43d66632dd68d35e2cf4e068576f8707f026a
karchx <kenethra.74@gmail.com>
2023-01-07 23:13:55 +0000

    fix: sym link

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,63 @@
-/etc/nixos/configuration.nix
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./modules/hyprland/default.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  services = {
+    xserver = {
+      enable = true;
+      displayManager = {
+        gdm.enable = true;
+        gdm.wayland = true;
+      };
+      desktopManager = {
+	plasma5.enable = true;
+      };
+    };
+  };
+
+  xdg = {
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+	xdg-desktop-portal-wlr
+	xdg-desktop-portal-gtk
+      ];
+      gtkUsePortal = true;
+    }; 
+  };
+ 
+  users.users.stivscience = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
+
+  hardware = {
+    opengl = { 
+      enable = true;
+      driSupport = true;
+    };
+  };
+  
+  system.stateVersion = "22.11"; # Did you read the comment?
+}
+
--- home.nix
+++ home.nix
@@ -1,1 +1,14 @@
-/home/stivscience/.config/nixpkgs/home.nix
+{ config, pkgs, ... }:
+
+{
+  home.username = "stivscience";
+  home.homeDirectory = "/home/stivscience";
+  home.stateVersion = "22.11";
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    tmux
+    alacritty
+    firefox
+  ];
+}
